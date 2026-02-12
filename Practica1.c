eb05b9f7766cc6899c3d8fcfbf7acda683ceb67c
Arturo-nh <inquisidor.uli@gmail.com>
2026-02-12 08:29:03 -0600

    Funciones

--- Practica1.c
+++ Practica1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main() {
-    printf("Hola soy Alan Bot\n");
+void intro() {
+        printf("Hola soy Alan Bot\n");
     float high;
     int age;
     char EC;
@@ -11,6 +11,56 @@ int main() {
     printf("Tamanyo de bits 1 = %d\n", sizeof(high));
     printf("Tamanyo de bits 2 = %d\n", sizeof(age));
     printf("Tamanyo de bits 3 = %d\n", sizeof(EC));
+    
+}
 
-	return 0;
+void finanzas() {
+
+    float balance;
+    float ingresos_m;
+    float gastos;
+    float meta_ahorro;
+    int bono_anual;
+
+    printf("Dame el balance de la cuenta, ingresos mensuales, Gasyos mensuales, Meta de ahorro \n y el bono anual en porcentaje (separados por un espacio)\n");
+    scanf("%f %f %f %f %d",&balance,&ingresos_m,&gastos,&meta_ahorro,&bono_anual);
+    balance=balance+ingresos_m-gastos;
+    printf("Cuenta en el primer mes = %f\n",balance);
+    balance=balance+ingresos_m-gastos-1200;
+    printf("Cuenta en el segundo mes, con gasto inesperado = %f\n",balance);
+    balance=balance+(10*(ingresos_m-gastos));
+    balance=(ingresos_m*(bono_anual/100))+balance;
+    printf("Cuenta despues de un anyo = %f\n",balance);
+    float meses;
+    meses=(balance-meta_ahorro)/(ingresos_m-gastos);
+
+}
+
+
+void operador_de_bits() {
+
+    int x=0;
+    int par=0;
+    int a=1;
+    int potencia=0;
+
+    printf("Dame un numero entero positivo: \n");
+    scanf("%d",&x);
+    int mask=00000000000000000000000000000001;
+    par = x&mask;
+    printf("Si es 0 es par si es 1 es impar: %d\n",par);
+    x<<=1;
+    printf("El numero multiplicado x2 es: %d\n",x);
+    a = x-a;
+    potencia=x&a;
+    printf("si es 0 es potencia, si es 1 no es potencia: %d",potencia);
+
+}
+
+int main() {
+
+    intro();
+    finanzas();
+    operador_de_bits();
+
 }
